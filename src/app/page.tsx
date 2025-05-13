b65e263de6c982db4ba808de2dc4b7f69ec7dee2
LegalBrian <legalbriandev@gmail.com>
2025-05-13 18:15:09 -0300

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,102 +2,80 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <main className="flex flex-col gap-5 row-start-2 items-center justify-between sm:items-start h-dvh">
+      <div className="text-white text-xs gap-3 flex items-center justify-center w-full py-4">
+        <button className="px-2 py-1 rounded-lg hover:bg-white hover:text-black transition duration-400 cursor-pointer">Inicio</button>
+        <button className="px-2 py-1 rounded-lg hover:bg-white hover:text-black transition duration-400 cursor-pointer">Experiencia</button>
+        <button className="px-2 py-1 rounded-lg hover:bg-white hover:text-black transition duration-400 cursor-pointer">Proyectos</button>
+      </div>
+      <div className="h-full w-full flex items-center justify-center gap-5">
+        <div className="text-white w-[450px] flex flex-col text-start gap-5">
+          <h1 className="font-bold text-7xl/normal text-shadow-black text-shadow-2xs opacity-80 typing-container">Brian Legal</h1>
+          <h2 className="font-bold text-2xl/normal text-shadow-black text-shadow-2xs opacity-80">Full Stack Developer</h2>
+          <p className="text-xs/normal text-shadow-black text-shadow-2xs opacity-80">Innovador Desarrollador con una sólida experiencia de más de dos años en desarrollo Full Stack implementando interfaces atractivas y funcionales, además de asegurar la escalabilidad y el mantenimiento óptimo de aplicaciones.</p>
+          <div className="flex flex-row gap-6">
+            <button className="group px-2 py-1 border text-white border-white rounded hover:bg-white flex flex-row gap-2 hover:text-black duration-300 cursor-pointer">
+              <Image
+                className="group-hover:invert"
+                src="/github.svg"
+                alt="elipse"
+                width={16}
+                height={16}
+              />
+              <p className="hidden group-hover:inline-block transition-opacity duration-500 text-xs">Github</p>
+            </button>
+            <button className="group px-2 py-1 border text-white border-white rounded hover:bg-white flex flex-row gap-2 hover:text-black duration-300 cursor-pointer">
+              <Image
+                className="group-hover:invert"
+                src="/linkedin.svg"
+                alt="elipse"
+                width={16}
+                height={16}
+              />
+              <p className="hidden group-hover:inline-block transition-opacity duration-500 text-xs">Linkedin</p>
+            </button>
+            <button className="group px-2 py-1 border text-white border-white rounded hover:bg-white flex flex-row gap-2 hover:text-black duration-300 cursor-pointer">
+              <Image
+                className="group-hover:invert"
+                src="/mail.svg"
+                alt="elipse"
+                width={16}
+                height={16}
+              />
+              <p className="hidden group-hover:inline-block transition-opacity duration-500 text-xs">Email</p>
+            </button>
+            <button className="group px-2 py-1 border text-white border-white rounded hover:bg-white flex flex-row gap-2 hover:text-black duration-300 cursor-pointer">
+              <Image
+                className="group-hover:invert"
+                src="/download.svg"
+                alt="elipse"
+                width={16}
+                height={16}
+              />
+              <p className="hidden group-hover:inline-block transition-opacity duration-500 text-xs">Descargar CV</p>
+            </button>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <div className="relative w-[500px] h-[500px] flex items-center justify-center">
           <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+            className="opacity-70 absolute spin"
+            src="/elipse.svg"
+            alt="elipse"
+            width={500}
+            height={500}
+            priority
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
           <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
+            className="relative opacity-85"
+            src="/image.svg"
+            alt="image"
+            width={360}
+            height={360}
+            priority
           />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        </div>
+      </div>
+    </main>
+
   );
 }
