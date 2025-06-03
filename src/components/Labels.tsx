ade0c53ac887957662fa9bba1261d68e0fe75279
LegalBrian <legalbriandev@gmail.com>
2025-06-02 21:14:55 -0300

    label fixed

--- src/components/Labels.tsx
+++ src/components/Labels.tsx
@@ -6,10 +6,10 @@ export default function Labels({
     list
 }: LabelsProps) {
     return (
-        <div className="flex flex-row flex-wrap gap-2 bg-black/70">
+        <div className="flex flex-row flex-wrap gap-2">
             {
                 list.map((i, k) => (
-                    <p key={k} className="px-2 py-1 border border-white/40 text-center rounded-md text-sm text-white/80">{i}</p>
+                    <p key={k} className="px-2 py-1 border border-white/40 text-center rounded-md text-sm text-white/80 bg-black/70">{i}</p>
                 ))
             }
         </div>
